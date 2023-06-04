5eca20b2b8535955e63f220a1dccf5158ddf66ae
JinZhangLab <zhangjin@mail.nankai.edu.cn>
2023-06-04 08:37:02 +0000

    bug fix

--- app/Index.py
+++ app/Index.py
@@ -1,7 +1,12 @@
+import os
+import sys
 import streamlit as st
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
 
 st.set_page_config(page_title="NIR Online-Home", page_icon=":house:", layout="wide")
 
-with open("Index.html",'r', encoding= 'unicode_escape') as f:
+with open(os.path.join(project_dir, "Index.html"),
+          mode = 'r', encoding= 'unicode_escape') as f:
     IndexContents = f.read()
 st.markdown(IndexContents, unsafe_allow_html=True)
