599a707877150b7e3e7d7c8f800240a5618404ba
JinZhangLab <zhangjin@mail.nankai.edu.cn>
2023-06-06 03:03:26 +0000

    bug fix

--- app/pages/5_Feature Selection.py
+++ app/pages/5_Feature Selection.py
@@ -1,11 +1,10 @@
-# Import streamlit and other libraries
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from pynir.utils import simulateNIR
 from pynir.Calibration import pls
 from pynir.FeatureSelection import MCUVE, RT, VC
-import pandas as pd
 from tools.display import plotSPC, plotRef_reg, plotVariableImportance, plotVariableSelection
 from tools.dataManipulation import download_csv_md
 
@@ -23,10 +22,8 @@ def featureSelection(X, y, options=None):
 
     if "PLS coefficients" in methods:
         pls_model = pls(n_comp_pls).fit(X.to_numpy(), y.to_numpy())
-        feature_selected["PLS coefficients"] = np.argsort(
-            np.abs(pls_model.model['B'][1:, n_comp_pls-1]))[::-1][:n_sel]
-        variable_importance["PLS coefficients"] = np.abs(
-            pls_model.model['B'][1:, n_comp_pls-1])
+        feature_selected["PLS coefficients"] = np.argsort(np.abs(pls_model.model['B'][1:, n_comp_pls-1]))[::-1][:n_sel]
+        variable_importance["PLS coefficients"] = np.abs(pls_model.model['B'][1:, n_comp_pls-1])
 
     if "VIP" in methods:
         pls_model = pls(n_comp_pls).fit(X.to_numpy(), y.to_numpy())
@@ -35,8 +32,7 @@ def featureSelection(X, y, options=None):
         variable_importance["VIP"] = vip
 
     if "MC-UVE" in methods:
-        mc_model = MCUVE(X.to_numpy(), y.to_numpy(),
-                         n_comp_pls, nrep=n_rep).fit()
+        mc_model = MCUVE(X.to_numpy(), y.to_numpy(), n_comp_pls, nrep=n_rep).fit()
         feature_selected["MC-UVE"] = mc_model.featureRank[:n_sel]
         variable_importance["MC-UVE"] = mc_model.criteria
 
@@ -51,14 +47,12 @@ def featureSelection(X, y, options=None):
         variable_importance["VC"] = vc_model.criteria
 
     # Create a DataFrame to store the feature selection results
-    FS = pd.DataFrame(data=np.full((len(feature_selected),
-                      X.shape[1]), False), index=feature_selected.keys(), columns=wv)
+    FS = pd.DataFrame(data=np.full((len(feature_selected), X.shape[1]), False), index=feature_selected.keys(), columns=wv)
     for method in feature_selected.keys():
         FS.loc[method].iloc[feature_selected[method]] = True
 
     # Create a DataFrame to store the variable importance values
-    Imp = pd.DataFrame(variable_importance, index=wv,
-                       columns=feature_selected.keys()).T
+    Imp = pd.DataFrame(variable_importance, index=wv, columns=feature_selected.keys()).T
 
     if "FS" in locals() and "Imp" in locals() and "X" in locals():
         # Plot the variable selection and variable importance
@@ -71,11 +65,12 @@ def featureSelection(X, y, options=None):
     return FS, Imp
 
 
-# Create a title and
+# Create a title and description
 st.title("Feature Selection for NIR Spectroscopy")
 st.markdown("### Upload your data or use our example.")
-use_example = st.radio(
-    "1.1", ["Example data 1", "Upload data manually"], label_visibility="collapsed")
+
+# Choose between example data or manual upload
+use_example = st.radio("Choose an option", ["Example data 1", "Upload data manually"])
 
 if use_example == "Example data 1":
     X, y, wv = simulateNIR()
@@ -93,7 +88,8 @@ if use_example == "Example data 1":
 
     with col2:
         plotRef_reg(y)
-elif use_example == "Upload data manually":
+
+else:
     tab1, tab2 = st.columns(2)
     with tab1:
         uploadX = st.file_uploader("Choose file for NIR spectra", type="csv")
@@ -102,8 +98,7 @@ elif use_example == "Upload data manually":
             wv = np.array(X.columns, dtype=float)
             plotSPC(X)
     with tab2:
-        uploadY = st.file_uploader(
-            "Choose file for reference values", type="csv")
+        uploadY = st.file_uploader("Choose file for reference values", type="csv")
         if uploadY is not None:
             y = pd.read_csv(uploadY, index_col=0)
             plotRef_reg(y)
@@ -121,12 +116,9 @@ if "X" in locals() and "y" in locals():
         method_vc = cols[0].checkbox("VC", value=True)
 
         # Perform feature selection using different methods
-        n_comp_pls = st.slider("Number of components", 1, int(
-            np.min(list(X.to_numpy().shape) + [10])), 1)
-        n_sel = st.slider("Number of selected features", 1,
-                          X.shape[1], round(X.shape[1] * 0.2))
-        n_rep = st.slider("Number of repetitions", 1 *
-                          X.shape[1], 10*X.shape[1], 2*X.shape[1])
+        n_comp_pls = st.slider("Number of components", 1, int(np.min(list(X.to_numpy().shape) + [10])), 1)
+        n_sel = st.slider("Number of selected features", 1, X.shape[1], round(X.shape[1] * 0.2))
+        n_rep = st.slider("Number of repetitions", 1 * X.shape[1], 10 * X.shape[1], 2 * X.shape[1])
 
     methods = []
     if method_pls:
@@ -140,26 +132,21 @@ if "X" in locals() and "y" in locals():
     if method_vc:
         methods.append("VC")
 
-    options = {"n_comp_pls": n_comp_pls, "n_sel": n_sel,
-               "n_rep": n_rep, "methods": methods}
+    options = {"n_comp_pls": n_comp_pls, "n_sel": n_sel, "n_rep": n_rep, "methods": methods}
 
     FS, Imp = featureSelection(X, y, options)
 
-# Apply the feature selection results to other spectral data uplsoaded
-
+# Apply the feature selection results to other spectral data uploaded
 if "FS" in locals():
     st.markdown("### Apply Feature Selection results to uploaded Spectra")
 
-    fs_method = st.radio("Apply feature selection",
-                         FS.index, label_visibility='collapsed')
+    fs_method = st.radio("Apply feature selection", FS.index)
 
-    upload_X_new = st.file_uploader(
-        "Choose file for NIR spectra to apply feature selection", type="csv")
+    upload_X_new = st.file_uploader("Choose file for NIR spectra to apply feature selection", type="csv")
     if upload_X_new is not None:
         X_new = pd.read_csv(upload_X_new, index_col=0)
         plotSPC(X_new, title="Spectra uploaded")
 
         X_new_fs = X_new.iloc[:, FS.loc[fs_method].to_numpy()]
 
-        download_csv_md(X_new_fs, fileName="X_fs",
-                        label="Download the spectra after feature selection")
+        download_csv_md(X_new_fs, fileName="X_fs", label="Download the spectra after feature selection")
